98cb1c6e0a97eebed88f92debe0ee61759e66430
Pedro1949 <142362192+Pedro1949@users.noreply.github.com>
2023-09-16 22:46:37 -0500

    Add files via upload

---
+++ 01.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declaración de variables
+        float x = 144;
+        float y = 999;
+
+        // Mostrar el valor de las variables
+        System.out.println("x es: " + x);
+        System.out.println("y es: " + y);
+
+        // Realizar operaciones aritméticas
+        float suma = x + y;
+        float resta = x - y;
+        float division = x / y; // La división de enteros devuelve un entero
+        float multiplicacion = x * y;
+
+        // Mostrar los resultados de las operaciones
+        System.out.println("La suma: " + suma);
+        System.out.println("La resta: " + resta);
+        System.out.println("La división: " + division);
+        System.out.println("La multiplicacion: " + multiplicacion);
+    }
+}
